9e8424ab9a02f72d69700cd035fb8c43cd8c4f36
kidstylex <chilizhiki@gmail.com>
2019-08-22 15:09:23 +0700

    + add, str, mul

--- fraction.py
+++ fraction.py
@@ -1,3 +1,4 @@
+import math
 class Fraction:
     """A fraction with a numerator and denominator and arithmetic operations.
 
@@ -12,21 +13,40 @@ class Fraction:
         """Initialize a new fraction with the given numerator
            and denominator (default 1).
         """
-        #TODO write this (and remove this TODO comment)
-        pass
 
-    #TODO Write the __add__ method, and remove this TODO comment.
+        if not isinstance(numerator, int):
+            raise TypeError("Numerator must be int.")
+
+        if not isinstance(denominator, int):
+            raise TypeError("Denominator must be int.")
+
+        self.gcd = math.gcd(numerator, denominator)
+        self.numerator = int(numerator / self.gcd)
+        self.denominator = int(denominator / self.gcd)
+
+        if self.denominator < 0:
+            self.numerator *= -1
+            self.denominator *= -1
+
     def __add__(self, frac):
         """Return the sum of two fractions as a new fraction.
            Use the standard formula  a/b + c/d = (ad+bc)/(b*d)
         """
-        pass
+        new_num = (self.numerator*frac.denominator) + (self.denominator*frac.numerator)
+        new_deno = self.denominator*frac.denominator
+        return Fraction(new_num, new_deno)
 
-    #TODO write __mul__ and __str__.  Verify __eq__ works with your code.
-    #Optional have fun and overload other operators such as 
-    # __sub__ for f-g
-    # __gt__  for f > g
-    # __neg__ for -f (negation)
+    def __mul__(self, frac):
+        return Fraction(self.numerator*frac.numerator, self.denominator*frac.denominator)
+
+    def __str__(self):
+        if self.denominator == 1:
+            return "{}".format(self.numerator)
+        return "{}/{}".format(self.numerator, self.denominator)
+
+    # def __sub__(self, frac):
+    # def __gt__(self, frac):
+    # def __neg__(self):
 
     def __eq__(self, frac):
         """Two fractions are equal if they have the same value.
